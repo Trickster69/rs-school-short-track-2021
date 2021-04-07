49864be20d2aafc20bf5407f2ccbb875b5ec445b
Sergei Demchuk <vinci.ru@mail.ru>
2021-04-07 10:21:43 +0300

    feat: add 5th task

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,18 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  let max = 0;
+  const arr = String(n).split('');
+  for (let i = 0; i < arr.length; i++) {
+    const arr2 = arr.slice();
+    arr2.splice(i, 1);
+    // console.log(arr2);
+    if (+arr2.join('') > max) {
+      max = +arr2.join('');
+    }
+  }
+  return max;
 }
 
 module.exports = deleteDigit;
