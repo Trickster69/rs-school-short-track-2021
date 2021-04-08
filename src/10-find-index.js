892cf2594d304ae2cc4b247c8c75227a1a2c5054
Sergei Demchuk <vinci.ru@mail.ru>
2021-04-08 12:38:00 +0300

    feat: add 10-11 task

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,15 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let b = 0;
+  for (let i = array.length; i > 0; i--) {
+    if (array[i] === value) {
+      b = i;
+      break;
+    }
+  }
+  return b;
 }
 
 module.exports = findIndex;
--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,33 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const obj = {};
+  const arr = str.split('');
+  for (let i = 0; i < arr.length; i++) {
+    if (obj[arr[i]] !== undefined) {
+      if ((arr[i + 1] !== arr[i]) && arr[i - 1] !== arr[i]) {
+        ++obj[arr[i].toUpperCase()];
+      } else {
+        ++obj[arr[i]];
+      }
+    } else {
+      obj[arr[i]] = 1;
+    }
+  }
+
+  const final = [];
+  Object.keys(obj).forEach((key) => {
+    if (obj[key] > 1) {
+      final.push(`${obj[key]}${key}`);
+    } else if (obj[key] <= 1) {
+      final.push(key);
+    } else {
+      final.push(key);
+    }
+  });
+
+  return final.join('').toLowerCase();
 }
 
 module.exports = encodeLine;
