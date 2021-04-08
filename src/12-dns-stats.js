588c5e8a79c055be7e8708f8897f57fe6845be14
Sergei Demchuk <vinci.ru@mail.ru>
2021-04-08 16:08:04 +0300

    feat: add 12 and 15 tasks

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,37 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const obj = {};
+  const final = {};
+  for (let i = 0; i < domains.length; i++) {
+    const domain = domains[i].split('.').reverse();
+    for (let b = 0; b < domain.length; b++) {
+      if (obj[domain[b]] !== undefined) {
+        ++obj[domain[b]];
+      } else {
+        obj[domain[b]] = 1;
+      }
+    }
+  }
+
+  const arr = [];
+  for (let i = 0; i < Object.keys(obj).length; i++) {
+    if (i === 0) {
+      arr.push(`.${Object.keys(obj)[0]}:${obj[Object.keys(obj)[i]]}`);
+    } else if (i === 1) {
+      arr.push(`.${Object.keys(obj)[0]}.${Object.keys(obj)[1]}:${obj[Object.keys(obj)[i]]}`);
+    } else if (i === 2) {
+      arr.push(`.${Object.keys(obj)[0]}.${Object.keys(obj)[1]}.${Object.keys(obj)[2]}:${obj[Object.keys(obj)[i]]}`);
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    const str = arr[i].split(':');
+    for (let b = 0; b < str.length; b++) {
+      final[str[0]] = +str[1];
+    }
+  }
+  return final;
 }
 
 module.exports = getDNSStats;
